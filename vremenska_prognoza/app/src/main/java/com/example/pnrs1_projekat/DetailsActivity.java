1ef0a0b6f4df25099ff69ea42d9a939510c47c09
miodrag997 <miodrag.pantic97@gmail.com>
2019-03-18 16:34:02 +0100

    zapocet prvi zadatak

--- vremenska_prognoza/app/src/main/java/com/example/pnrs1_projekat/DetailsActivity.java
+++ vremenska_prognoza/app/src/main/java/com/example/pnrs1_projekat/DetailsActivity.java
@@ -1,13 +1,25 @@
 package com.example.pnrs1_projekat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    TextView location;
+    String s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        location = findViewById(R.id.location);
+        Intent myIntent = getIntent();
+        Bundle b = myIntent.getExtras();
+        s = b.getString("location");
+        s = location.getText()+s;
+        location.setText(s);
     }
 }
