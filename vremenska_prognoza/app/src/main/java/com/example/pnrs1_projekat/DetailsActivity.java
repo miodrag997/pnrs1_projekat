7b0d4e14c8154a4756ff8f0f79dbc0f6fe8ab84d
miodrag997 <miodrag.pantic97@gmail.com>
2019-03-20 00:21:04 +0100

    sve funkcije rade potrebno je samo zavrsiti spiner i uljepsati malo

--- vremenska_prognoza/app/src/main/java/com/example/pnrs1_projekat/DetailsActivity.java
+++ vremenska_prognoza/app/src/main/java/com/example/pnrs1_projekat/DetailsActivity.java
@@ -3,12 +3,27 @@ package com.example.pnrs1_projekat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class DetailsActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.view.View.GONE;
+
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView location;
+    TextView day;
     String s;
+    FrameLayout frameLayout;
+    LinearLayout temperatureLayout, sunRiseSetLayout, windLayout;
+    Button temperatureButton, sunRiseSetButton, windButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +36,41 @@ public class DetailsActivity extends AppCompatActivity {
         s = b.getString("location");
         s = location.getText()+s;
         location.setText(s);
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        Date d = new Date();
+        String dayOfTheWeek = sdf.format(d);
+        day = findViewById(R.id.day);
+        day.setText(dayOfTheWeek);
+
+        temperatureLayout = findViewById(R.id.temperatureLayout);
+        sunRiseSetLayout = findViewById(R.id.sunRiseSetLayout);
+        windLayout = findViewById(R.id.windLayout);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        temperatureLayout.setVisibility(GONE);
+        sunRiseSetLayout.setVisibility(GONE);
+        windLayout.setVisibility(GONE);
+
+        temperatureButton = findViewById(R.id.temperatureButton);
+        sunRiseSetButton = findViewById(R.id.sunRiseSetButton);
+        windButton = findViewById(R.id.windButton);
+
+        temperatureButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.temperatureButton:
+                temperatureLayout.setVisibility(v.VISIBLE);
+
+                break;
+            case R.id.sunRiseSetButton:
+                break;
+            case R.id.windButton:
+                break;
+        }
     }
 }
